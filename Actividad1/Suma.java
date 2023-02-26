d87ffe8d0c1ea3a9ab6b57892ab3a18ac2c59571
Elver Noe Tunubala Yalanda <entunubala@tcc.com.co>
2023-02-25 21:00:07 -0500

    Ejercicio Resuelto No 5

---
+++ Actividad1/Suma.java
@@ -0,0 +1,12 @@
+public class Suma {
+    public static void main(String[] args) {
+        int suma=0;
+        int x =20;
+        suma = suma + x;
+        int y = 40;
+        x = x + y*y;
+        suma = suma + x/y;
+        
+        System.out.println("El valor de la suma es: " + suma);
+    }
+}
