fbf02e8f66eb708645ced9cabb33ca7a69e0ee15
Elver Noe Tunubala Yalanda <entunubala@tcc.com.co>
2023-02-25 21:11:54 -0500

    Ejercicio Propuesto No 14

---
+++ Actividad1/Circulo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Circulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a,r,l; // área,radio y longitud
+
+        System.out.print("Introduce el radio de un circulo: ");
+        r = sc.nextDouble();
+        a=Math.PI*(r*r);
+        l=Math.PI*(r+r);
+
+        System.out.println("El área del circulo de radio " + r+ " es: " + a);
+        System.out.println("La longitud de la circunferencia es: "+ l);
+    }
+}
--- Actividad1/Numero.java
+++ Actividad1/Numero.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Numero {
     public static void main(String[] args) {
         int numero,cuadrado,cubo;
